03aee5f6729dc4ad86a4a052ce659fde54f1331b
Nikita <jack.miles220800@gmail.com>
2019-08-31 02:24:43 +0300

    step two

---
+++ js/_custom.js
@@ -0,0 +1,12 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+	
+	var carousel = $("#carousel").flipster({
+        style: 'carousel',
+        spacing: -0.5,
+        nav: false,
+        buttons:   true,
+    });
+
+
+});
